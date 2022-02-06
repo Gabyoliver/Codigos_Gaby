35f7c769f2d2a2cf842ff23a107ed0e8e460f67d
Gabrielly Oliveira <91494281+Gabyoliver606@users.noreply.github.com>
2022-02-06 11:30:14 -0300

    Update cadastro_banco.cpp

--- 14_Cadastro/cadastro_banco.cpp
+++ 14_Cadastro/cadastro_banco.cpp
@@ -5,7 +5,7 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
-
+#include <iomanip>
 
 using namespace std;
 
@@ -16,224 +16,236 @@ protected:
     float balanco{0.00};
     string tipo{""}; // Corrente ou Poupança
 public:
-    Conta(int id = 0, string clienteId = "") : id(id), clienteId(clienteId) {};
-   
-    int getId() {
-        return id;
-    }
+    Conta(int id, string clienteId) : id(id), clienteId(clienteId) {};
 
     float getBalanco() const {
         return balanco;
     }
-    string getTipo() const {
-        return tipo;
-    }
+
     string getClienteId() const {
         return clienteId;
     }
 
-    virtual void deposito (float valor) {
+    int getId() {
+        return id;
+    }
+
+    string getTipo() const {
+        return tipo;
+    }
+
+    virtual void depositar(float valor) {
         balanco += valor;
-}
+    }
 
-    virtual void atualizacaoMensal () {}
+    virtual void monthlyUpdate() {}
 
-    virtual void transferencia (Conta &destino, float valor) {
+    virtual void transferir(Conta &conta, float valor) {
+        if(valor > balanco) {
+            throw invalid_argument("Saldo insuficiente");
+        }
         balanco -= valor;
-        destino.balanco += valor;
-}
+        conta.depositar(valor);
+    }
+
+    virtual void sacar(float valor) {
+        if(valor > balanco) {
+            throw invalid_argument("Saldo insuficiente");
+        }
+        balanco -= valor;
+    }
 
     friend ostream& operator<<(ostream& os, Conta* conta) {
-        os << conta->getId() << ":" << conta->getClienteId() << ":";
-        os << conta->getBalanco() << ":" << conta->getTipo() << endl;
+        os << conta->getId() << " : " << conta->getClienteId() << " : "; 
+        os << fixed << setprecision(2) << conta->getBalanco();
+        os << " : " << conta->getTipo() << endl;
         return os;
     }
 };
-
-class ContaCorrente : public Conta {
-public:
-    ContaCorrente(int id = 0, string clienteId = "") : Conta(id, clienteId) {
-        tipo = "Corrente";
-    }
-    
-    virtual void atualizacaoMensal(){
-        balanco -= 20;
-    } 
-
-}; 
-
-class ContaPoupanca : public Conta {
-public:
-    ContaPoupanca(int id = 0, string clienteId = "") : Conta(id, clienteId) {
-        tipo = "Poupança";
-    }
-    virtual void atualizacaoMensal(){
-        balanco += balanco * 0.01;
-    }
-
-};
-
-class Cliente {
-private:
-    string clienteId;
-    vector<shared_ptr<Conta>> conta;
-public:
-    Cliente(string clienteId = "", int id = 0) : clienteId{clienteId} {
-        this->adicionaConta(make_shared<ContaCorrente>(id, clienteId));
-        this->adicionaConta(make_shared<ContaPoupanca>((id + 1), clienteId));    
-    }
-    void adicionaConta(shared_ptr<Conta> conta){
-        this->conta.push_back(conta);
-    }
-
-    friend ostream& operator<<(ostream& os, const Cliente* cliente) {
-        os << cliente->clienteId << ":" << endl;
-        for (auto conta : cliente->conta) {
-            os << conta << endl;
+    class ContaPoupanca : public Conta {
+    public:
+        ContaPoupanca(int id, string clienteId) : Conta(id, clienteId) {
+            tipo = "Poupança";
         }
-        return os;
+
+        virtual void monthlyUpdate() {
+            balanco += balanco * 0.01;
+        }
+    };
+
+    class ContaCorrente : public Conta {
+    public:
+        ContaCorrente(int id, string clienteId) : Conta(id, clienteId) {
+            tipo = "Corrente";
+        }
+
+        virtual void monthlyUpdate() {
+            balanco -= 20;
+        }
+    };
+
+    class Cliente {
+        vector<shared_ptr<Conta>> contas;
+        string clienteId;
+    public:
+        Cliente(string clienteId = "", int id = 0) : clienteId(clienteId) {
+
+            this->addConta(shared_ptr<Conta>(new ContaCorrente(id, clienteId)));
+            this->addConta(shared_ptr<Conta>(new ContaPoupanca(id, clienteId)));
     }
 
-    vector<shared_ptr<Conta>> getContas() const {
-        return conta;
+    void addConta(shared_ptr<Conta> conta) {
+        contas.push_back(conta);
     }
 
-    string getCliente() const {
+    friend ostream& operator<<(ostream& os, Cliente* cliente) {
+       auto contas = cliente->getConta();
+       os << cliente->clienteId << "[" << endl;
+         for(int i = 0; i < (int) contas.size(); i++) {
+             if(i != (int) contas.size() - 1)
+                 os << contas[i]->getId() << ", ";
+              else 
+                 os << contas[i]->getId() << "]" << endl;
+             }
+            return os;
+        }
+
+    vector<shared_ptr<Conta>> getConta() const {
+        return contas;
+    }
+
+    string getClienteId() const {
         return clienteId;
     }
 
-    void setConta(shared_ptr<Conta> conta) {
-        this->conta.push_back(conta);
+    void setConta(vector<shared_ptr<Conta>> contas) {
+        this->contas = contas;
     }
 
     void setClienteId(string clienteId) {
         this->clienteId = clienteId;
     }
+}; 
 
-};
+    class Banco {
+       map<int, shared_ptr<Conta>> contas;
+       map<string, shared_ptr<Cliente>> clientes;
 
-class Banco {
-    map<string, shared_ptr<Cliente>> clientes;
-    map<int, shared_ptr<Conta>> conta;
+       Conta* getConta(int id) {
+           return this->contas[id].get();
+       }
 
-    Conta* getConta(int id) {
-        return this->conta[id].get();
-    }
-
-    void existe(int conta) {
-        if ((int) this->conta.size() <= conta)
-            throw invalid_argument("Conta não existe");
+       void existeConta(int id) {
+           if(this->contas.find(id) == this->contas.end()) 
+               throw invalid_argument("Conta não existe");
         }
-    
 public:
     Banco() {};
 
-    void adicionaCliente(string clienteId) {
+    void addConta(string clienteId){
         for(auto conta : clientes) {
-            if(conta.first == clienteId) {
+            if(conta.first == clienteId)
                 throw invalid_argument("Conta já existe");
-            }
         }
-        
-        clientes.insert(pair<string, shared_ptr<Cliente>>(clienteId, make_shared<Cliente>(clienteId)));
 
+        int size {(int) contas.size()};
+
+        auto cliente {make_shared<Cliente>(clienteId, size)};
+        clientes.insert({clienteId, cliente});
+
+        auto contas = cliente->getConta();
+        for(int i{0}; i < (int) contas.size(); i++) {
+            this->contas.insert({contas[i]->getId(), contas[i]});
+            size++;
+        }
     }
     
-    void deposito(int idConta, float valor) {
-        auto conta = getConta(idConta);
-        if(conta == nullptr) {
+    void depositar(int id, float valor) {
+        auto conta = getConta(id);
+        if((int)this->contas.size() <= id)
             throw invalid_argument("Conta não existe");
-        } else {
-            conta->deposito(valor);
+        else
+            conta->depositar(valor);
+    }
+
+    void monthlyUpdate() {
+        for(auto conta : contas) {
+            conta.second->monthlyUpdate();
         }
     }
 
-    void atualizacaoMensal() {
-        for (auto conta : conta) {
-            conta.second->atualizacaoMensal();
-        }
+    void transferir(int contaOrigem, int contaDestino, float valor) {
+        existeConta(contaOrigem);
+        existeConta(contaDestino);
+        auto conta = getConta(contaOrigem);
+        
+        conta->transferir(*getConta(contaDestino), valor);
     }
 
-    void transferencia(int idOrigem, int idDestino, float valor) {
-        existe(idOrigem);
-        existe(idDestino);
-        auto conta = getConta(idOrigem);
-
-        conta->transferencia( *(this->conta[idDestino]), valor);
-    }
-
-    void saque(int id, float valor) {
-        existe(id);
+    void sacar(int id, float valor) {
+        existeConta(id);
         auto conta = getConta(id);
 
-        conta->transferencia( *(this->conta[id]), valor);
-
+        conta->sacar(valor);
     }
 
-    friend ostream& operator<<(ostream& os, const Banco* banco) {
+    friend ostream& operator<<(ostream& os, Banco* banco) {
         os << "Clientes: " << endl;
-        for (auto cliente : banco->clientes) {
-            os << "-" << (cliente.second) << endl;
+
+        for(auto cliente : banco->clientes) {
+            os << cliente.second << endl;
         }
-            os << "Contas: " << endl;
-        for (auto conta : banco->conta) {
-            int{0};
-            os << (conta.second) << endl;
+
+        os << "Contas: " << endl;
+
+        for(auto conta : banco->contas) {
+            os << conta.second << endl;
         }
         return os;
     }
 };
 
-
 int main() {
     Banco* banco = new Banco();
-
-     while (true){
-        string line, cmd;
+    while (true){
+        string cmd;
         cin >> cmd;
-        getline(cin, line);
-        stringstream ss(line);
-
-     try {
-         if (cmd == "adicionaConta") {
-            string tipo;
+        if (cmd == "add"){
             string clienteId;
-            int id;
-            ss >> clienteId >> id;
-        } else if (cmd == "adicionaCliente") {
-            string clienteId;
-            ss >> clienteId;
-            banco->adicionaCliente(clienteId);
-        } else if (cmd == "deposito") {
-            int idConta;
-            float valor;
-            ss >> idConta >> valor;
-            banco->deposito(idConta, valor);
-        } else if (cmd == "atualizacaoMensal") {
-            banco->atualizacaoMensal();
-        } else if (cmd == "transferencia") {
-            int idOrigem, idDestino;
-            float valor;
-            ss >> idOrigem >> idDestino >> valor;
-            banco->transferencia(idOrigem, idDestino, valor);
-        } else if (cmd == "saque") {
-            int id;
-            float valor;
-            ss >> id >> valor;
-            banco->saque(id, valor);
-        } else if (cmd == "fim") {
-            break;
-        } else {
-            throw invalid_argument("Comando inválido");
+            cin >> clienteId;
+            banco->addConta(clienteId);
         }
-    } catch (invalid_argument& e) {
-        cout << e.what() << endl;
+        else if (cmd == "depositar"){
+            int id;
+            float valor;
+            cin >> id >> valor;
+            banco->depositar(id, valor);
+        }
+        else if (cmd == "transferir"){
+            int contaOrigem, contaDestino;
+            float valor;
+            cin >> contaOrigem >> contaDestino >> valor;
+            banco->transferir(contaOrigem, contaDestino, valor);
+        }
+        else if (cmd == "sacar"){
+            int id;
+            float valor;
+            cin >> id >> valor;
+            banco->sacar(id, valor);
+        }
+        else if (cmd == "monthlyUpdate"){
+            banco->monthlyUpdate();
+        }
+        else if (cmd == "show"){
+            cout << banco << endl;
+        }
+        else if (cmd == "exit"){
+            break;
+        }
+        else{
+            cout << "Comando inválido" << endl;
+        }
     }
-}
-
     delete banco;
-
     return 0;
 }
-
-       
